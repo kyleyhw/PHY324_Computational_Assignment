68f1ab5b533c010dbbc6531375a676dbbfb1e243
Kyle <kyleyhw@gmail.com>
2023-01-19 15:04:36 -0500

    fixed exercise_3 took absolute value too early mistake, widened filter Gaussian at frequency 0.14 (isolated peak) in attempt to avoid missing neighboring points to peak since peak contains at least 2 points >2000 amplitude

--- exercise_3.py
+++ exercise_3.py
@@ -56,10 +56,10 @@ class Exercise3():
         self.positions = self.data_from_file
         self.times = np.arange(len(self.positions))
 
-        self.amplitudes = np.abs(np.fft.fft(self.positions))
+        self.amplitudes = np.fft.fft(self.positions)
         self.frequencies = np.fft.fftfreq(self.number_of_points, self.spacing)
 
-        self.gaussian_filter_parameters_array = np.array([(0.059, 0.00004), (0.077, 0.00004), (0.14, 0.00004)])
+        self.gaussian_filter_parameters_array = np.array([(0.059, 0.00004), (0.077, 0.00004), (0.14, 0.0001)])
         self.symmetrical_gaussians = np.array([GaussianSymmetricalAboutZero(*params) for params in self.gaussian_filter_parameters_array])
 
         self.filter_function = Superpose(self.symmetrical_gaussians)
@@ -74,25 +74,23 @@ class Exercise3():
 
 
     def plot(self):
-        fig, ( (ax1, ax2), (ax3, ax4), (ax5, ax6) ) = plt.subplots(3, 2, sharex=False, sharey=False)
+        fig, ( (ax1, ax2), (ax3, ax4), (ax5, ax6) ) = plt.subplots(3, 2, sharex='col', sharey=False)
 
-        ax1.plot(self.times, self.positions) # position-time plot
-        ax1.plot(self.times, self.cleaned_positions, color='orange')
-        ax1.set_xlim(0, 200)
+        ax1.plot(self.times, self.positions) # position-time plot of noisy data
+        ax1.plot(self.times, self.cleaned_positions, color='orange') # position-time plot of cleaned data
+        ax1.set_xlim()
 
         ax3.plot(self.times, self.positions - self.cleaned_positions) # residuals
-        ax3.set_xlim(0, 200)
 
         ax5.plot(self.times, self.best_guess_function(self.times)) # best guess
-        ax5.set_xlim(0, 200)
 
 
 
-        ax2.plot(self.frequencies, self.amplitudes) # amplitude-frequency plot
+        ax2.plot(self.frequencies, np.abs(self.amplitudes)) # amplitude-frequency plot
 
         ax4.plot(self.frequencies, self.filter_function(self.frequencies)) # filter function plot
 
-        ax6.plot(self.frequencies, self.filtered_data) # filtered amplitude-frequencies plot
+        ax6.plot(self.frequencies, np.abs(self.filtered_data)) # filtered amplitude-frequencies plot
 
 
         plt.show()
--- main.py
+++ main.py
@@ -6,7 +6,7 @@ exercise_1 = exercise_1.Exercise1()
 # exercise_1.plot()
 
 exercise_3 = exercise_3.Exercise3()
-# exercise_3.plot()
+exercise_3.plot()
 
 exercise_4 = exercise_4.Exercise4()
-exercise_4.plot()
+# exercise_4.plot()
